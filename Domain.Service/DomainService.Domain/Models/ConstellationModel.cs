414497521ca344d0a167b754e2846e37a458b144
Jeremy Chionglo <jpchionglo@gmail.com>
2020-08-19 14:48:02 -0500

    Organized domain service into 1 folder

---
+++ Domain.Service/DomainService.Domain/Models/ConstellationModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DomainService.Domain.Models
+{
+
+    public class ConstellationModel : AModel
+    {
+
+      [NotMapped]
+      public List<StarModel> Stars {get;set;}
+
+      public string Description {get;set;}
+
+    }
+
+}
