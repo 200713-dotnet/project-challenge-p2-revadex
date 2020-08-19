647408030b2c3191242ad038ed4bf28fd83601fa
Jeremy Chionglo <jpchionglo@gmail.com>
2020-08-19 14:40:14 -0500

    Added List to Constellation and Solar System Entities (not mapped in DB), finished DomainServiceRepository

--- DomainService.Domain/Models/ConstellationModel.cs
+++ DomainService.Domain/Models/ConstellationModel.cs
@@ -1,10 +1,15 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DomainService.Domain.Models
 {
 
     public class ConstellationModel : AModel
     {
+
+      [NotMapped]
+      public List<StarModel> Stars {get;set;}
 
       public string Description {get;set;}
 
--- DomainService.Domain/Models/SolarSystemModel.cs
+++ DomainService.Domain/Models/SolarSystemModel.cs
@@ -1,10 +1,15 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DomainService.Domain.Models
 {
 
     public class SolarSystemModel : AModel
     {
+
+      [NotMapped]
+      public List<PlanetModel> Planets {get;set;}
 
       public string Description {get;set;}
 
