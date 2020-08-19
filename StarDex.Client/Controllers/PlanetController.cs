afa0deb7af6c4a513c20bf5f9c194e395c21137c
kyle dick <kyle.i.dick@outlook.com>
2020-08-19 14:02:34 -0400

    Added button template for stars

---
+++ StarDex.Client/Controllers/PlanetController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace StarDex.Client.Controllers {
+    public class PlanetController : Controller {
+      public string Test() {
+        return "A planet";
+      }
+    }
+}
