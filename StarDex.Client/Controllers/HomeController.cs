89a38273f656db3050d00c6c0bb846be4692d1ba
kyle dick <kyle.i.dick@outlook.com>
2020-08-19 13:30:05 -0400

    Added controllers; Removed useless text in HomeController and fixed formatting

--- StarDex.Client/Controllers/HomeController.cs
+++ StarDex.Client/Controllers/HomeController.cs
@@ -1,41 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using StarDex.Client.Models;
 
-namespace StarDex.Client.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace StarDex.Client.Controllers {
+    public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
+        public HomeController(ILogger<HomeController> logger) {
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
+        public IActionResult Index() {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
+        public IActionResult Privacy() {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
+        public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        // public FileResult Img() {
-        //   return File.Append();
-        // }
     }
 }
