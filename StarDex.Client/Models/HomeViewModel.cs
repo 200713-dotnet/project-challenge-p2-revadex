07f0a29beb4413866822c3f2bd5598f1efc0c7d8
kyle dick <kyle.i.dick@outlook.com>
2020-08-19 20:37:12 -0400

    Removed commented code

--- StarDex.Client/Models/HomeViewModel.cs
+++ StarDex.Client/Models/HomeViewModel.cs
@@ -2,8 +2,6 @@ using System.Collections.Generic;
 
 namespace StarDex.Client.Models {
   public class HomeViewModel {
-    // public List<ConstellationButtonModel> north { get; set; }
-    // public List<ConstellationButtonModel> south { get; set; }
     public List<ConstellationButtonModel> stars;
     public string name { get; set; }
   }
