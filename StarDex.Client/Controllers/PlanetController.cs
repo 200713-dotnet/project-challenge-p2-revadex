649ecd52a2e343f59f868036167557350384d849
kyle dick <kyle.i.dick@outlook.com>
2020-08-21 11:39:00 -0400

    Connected planet view to planet API; Changed planet view model to take doubles, not ints; Added planet view;

--- StarDex.Client/Controllers/PlanetController.cs
+++ StarDex.Client/Controllers/PlanetController.cs
@@ -1,10 +1,26 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using StarDex.Client.Models;
 
 namespace StarDex.Client.Controllers {
     public class PlanetController : Controller {
       [HttpGet]
-      public string Get(string name) {
-        return name;
+      public IActionResult Get(string name) {
+        string jsonString = null;
+        HttpWebRequest request = (HttpWebRequest) WebRequest.Create($"https://domainserviceapi.azurewebsites.net/api/Planet/{name}");
+        using (HttpWebResponse response = (HttpWebResponse) request.GetResponse()) {
+          using (Stream stream = response.GetResponseStream()) {
+            using (StreamReader reader = new StreamReader(stream)) {
+              jsonString = reader.ReadToEnd();
+            }
+          }
+        }
+        PlanetViewModel model = JsonConvert.DeserializeObject<PlanetViewModel>(jsonString);
+        return View("Planet", model);
       }
     }
 }
